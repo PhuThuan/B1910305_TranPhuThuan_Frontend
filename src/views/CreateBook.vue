8d28bba218db976e6c82603546471213609a5d8d
PhuThuan <phuthuan1910@gmail.com>
2023-04-07 21:50:34 +0700

    Tao trang them lien he

---
+++ src/views/CreateBook.vue
@@ -0,0 +1,36 @@
+<template>
+    <div v-if="contact" class="page">
+        <h4>Tạo liên hệ</h4>
+        <ContactForm :contact="contact" @submit:contact="createContact" />
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+import ContactForm from '../components/ContactForm.vue';
+import contactService from '../services/contact.service';
+export default {
+    components: {
+        ContactForm
+    },
+
+    data() {
+        return {
+            contact: {},
+            message: ""
+        }
+    },
+    methods: {
+        async createContact(data) {
+            try {
+                await contactService.create(data)
+                this.message = "Liên hệ được tạo"
+            }
+            catch (error) {
+                console.log(error)
+            }
+        }
+    }
+}
+</script>
+
+<style></style>
